b8f3efc94ed6b7915a92e05076744d0e5f168107
adairjk <adairjk@gmail.com>
2015-05-11 20:18:08 -0500

    making a game of it

--- app/assets/javascripts/blocks.js
+++ app/assets/javascripts/blocks.js
@@ -4,16 +4,45 @@ $(document).ready(function(){
 
     $('div').click(function() {
         $(this).effect('bounce', {times:3}, 500);
+
+        var row = $(this).data('row');
+        var column = $(this).data('col');
+
+        toggleAbove(row, column);
+        toggleBelow(row, column);
+        toggleLeft(row, column);
+        toggleRight(row, column);
+        
+        toggleOnOff(this);
     });
 
-    $('.red').click(function(){
-        $(this).toggleClass('green');
-    });
+    var toggleOnOff = function(element){
+        $(element).toggleClass('on');
+    };
 
-    $('.monkey').click(function(){
-        $(this).animate({ opacity: '0' });
-        $(this).delay(500).show().animate({ top: 305, opacity: '1' }, {duration: 1000, easing: 'easeOutBounce'});
-    });
+    var toggleAbove = function(row, column) {
+        if (row != 0) {
+            toggleOnOff($("div[data-col='" + column + "'][data-row='" + (row - 1) + "']"));
+        }
+    };
 
+    var toggleBelow = function(row, column) {
+        if (row != 4) {
+            toggleOnOff($("div[data-col='" + column + "'][data-row='" + (row + 1) + "']"));
+        }
+    };
 
+    var toggleLeft = function(row, column) {
+        if (column != 0) {
+            toggleOnOff($("div[data-col='" + (column - 1) + "'][data-row='" + row + "']"));
+        }
+    };
+
+    var toggleRight = function(row, column) {
+        if (column != 4) {
+            toggleOnOff($("div[data-col='" + (column + 1) + "'][data-row='" + row + "']"));
+        }
+    };
+
+    toggleOnOff($("div[data-col='2'][data-row='2']"));
 });
