863ba7a876eb57c42c9e44001a20256dcc35951a
adairjk <adairjk@gmail.com>
2015-05-12 20:24:47 -0500

    mixing things up a bit

--- app/assets/javascripts/blocks.js
+++ app/assets/javascripts/blocks.js
@@ -2,8 +2,9 @@
 
 $(document).ready(function(){
 
-    $('div').click(function() {
-        $(this).effect('bounce', {times:3}, 500);
+    var score = 0;
+
+    $('.block').click(function() {
 
         var row = $(this).data('row');
         var column = $(this).data('col');
@@ -12,37 +13,99 @@ $(document).ready(function(){
         toggleBelow(row, column);
         toggleLeft(row, column);
         toggleRight(row, column);
-        
-        toggleOnOff(this);
+
+        toggle(this);
+        addScore();
+        isGameOver();
     });
 
-    var toggleOnOff = function(element){
+    var toggle = function(element){
         $(element).toggleClass('on');
     };
 
     var toggleAbove = function(row, column) {
         if (row != 0) {
-            toggleOnOff($("div[data-col='" + column + "'][data-row='" + (row - 1) + "']"));
+            toggle($("div[data-col='" + column + "'][data-row='" + (row - 1) + "']"));
         }
     };
 
     var toggleBelow = function(row, column) {
         if (row != 4) {
-            toggleOnOff($("div[data-col='" + column + "'][data-row='" + (row + 1) + "']"));
+            toggle($("div[data-col='" + column + "'][data-row='" + (row + 1) + "']"));
         }
     };
 
     var toggleLeft = function(row, column) {
         if (column != 0) {
-            toggleOnOff($("div[data-col='" + (column - 1) + "'][data-row='" + row + "']"));
+            toggle($("div[data-col='" + (column - 1) + "'][data-row='" + row + "']"));
         }
     };
 
     var toggleRight = function(row, column) {
         if (column != 4) {
-            toggleOnOff($("div[data-col='" + (column + 1) + "'][data-row='" + row + "']"));
+            toggle($("div[data-col='" + (column + 1) + "'][data-row='" + row + "']"));
         }
     };
 
-    toggleOnOff($("div[data-col='2'][data-row='2']"));
+    var toggleRight = function(row, column) {
+        if (column != 4) {
+            toggle($("div[data-col='" + (column + 1) + "'][data-row='" + row + "']"));
+        }
+    };
+
+    var addScore = function() {
+        score = score + 1;
+        $('#score').text(score);
+    };
+
+    var subtractScore = function() {
+        score = score - 1;
+        $('#score').text(score);
+    };
+
+    var clearBoard = function() {
+        $('.block').removeClass('on');
+    };
+
+    var randomNumber = function() {
+        return Math.floor(Math.random() * 5);
+    };
+
+    var randomPair = function() {
+        var rand1 = randomNumber();
+        var rand2 = randomNumber();
+        return [rand1,rand2];
+    };
+
+    var setupGame = function(array) {
+        clearBoard();
+        for (var i in array) {
+            $("div[data-col='" + array[i][0] + "'][data-row='" + array[i][1] + "']").click();
+            subtractScore();
+            randomNumber();
+        }
+    };
+
+    var buildArray = function() {
+        var array = [];
+        for(var i = 0; i < 5; i++) {
+            array.push(randomPair());
+        }
+        return array;
+    };
+
+    var isGameOver = function() {
+
+        if ($('.on').size() === 0) {
+            $('.block').hide("explode", {pieces: 9 }, 1000);
+        }
+    };
+
+    $('#reset').click(function() {
+        $('.block').show();
+        score = 0;
+        setupGame(buildArray());
+    });
+
+    setupGame(buildArray());
 });
